ab39f68b056fb29e1bc3f533062dea27aec8dc8c
binglei.hou <>
2023-05-23 10:54:16 +0800

    no message

--- orm/unsafe/unsafe.go
+++ orm/unsafe/unsafe.go
@@ -1,1 +1,17 @@
-package unsafe_test
+package unsafe
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func PrintFieldOffset(entity any) {
+
+	typ := reflect.TypeOf(entity)
+	numField := typ.NumField()
+	for i := 0; i < numField; i++ {
+		field := typ.Field(i)
+		fmt.Println(field.Offset)
+	}
+
+}
--- orm/unsafe/unsafe_test.go
+++ orm/unsafe/unsafe_test.go
@@ -1,1 +1,35 @@
-package unsafe_test
+package unsafe
+
+import (
+	"testing"
+)
+
+type User struct {
+	// 0
+	Name string
+	// 16
+	Age int32
+	// 20
+	Age2 int32
+	// 24
+	Address []string
+}
+
+func TestPrintFieldOffset(t *testing.T) {
+	testCases := []struct {
+		name   string
+		entity any
+	}{
+		{
+			name:   "user",
+			entity: User{},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			PrintFieldOffset(tc.entity)
+		})
+	}
+
+}
